cefc926d2fc16591c3bdc8b6174e285d30b41646
FredericoSilva015 <fredsilva015@gmail.com>
2017-04-25 18:23:19 +0100

    first commit

---
+++ js/solutionController.js
@@ -0,0 +1,36 @@
+var app = angular.module('app', ['ngSanitize']);
+
+app.factory('terms', function($http) {
+    var obj = {
+        content: null
+    };
+
+    $http.get('terms.json').success(function(data) {
+        obj.content = data;
+    });
+
+    return obj;
+});
+app.filter('check', ['$sce', function($sce) {
+    return function(obj) {
+        if (angular.isObject(obj)) {
+            var nested = '<li>' + obj.content_main + '</li>';
+            if ('subcontent' in obj) {
+                nested += '<ul>';
+                angular.forEach(obj.subcontent, function(item) {
+                    nested += '<li>' + item + '</li>';
+                });
+                nested += '</ul>';
+            }
+
+            return $sce.trustAsHtml(nested);
+        }
+        return $sce.trustAsHtml(obj);
+    };
+}]);
+
+app.controller('myCtrl', function($scope, terms) {
+    $scope.data = terms;
+    var use = $scope.data;
+    console.log($scope.data);
+});;
